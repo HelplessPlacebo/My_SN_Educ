543e8d3e26884d5a88ab32eb1d02b714f83507d2
Admin <xndqhacj@yandex.ru>
2019-12-01 00:30:04 +0200

    added new version

--- static/js/3.93a05a9e.chunk.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{302:function(n,e,t){},303:function(n,e,t){"use strict";t.r(e);var i=t(0),a=t.n(i),c=(t(302),t(11)),l=t(43);e.default=Object(c.d)(l.a)(function(n){return a.a.createElement("div",null,"will be available in the next version")})}}]);
-//# sourceMappingURL=3.93a05a9e.chunk.js.map
